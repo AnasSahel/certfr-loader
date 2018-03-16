e40c58263becedc4c414683b12ad2a7e63954d3a
Anas <anas.sahel@gmail.com>
2018-03-17 00:12:46 +0100

    update gitignore and npmignore

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL0FuYXMvV29ya3NwYWNlL2NlcnRmci9jZXJ0ZnItbG9hZGVyL2xpYi8iLCJzb3VyY2VzIjpbImluZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
