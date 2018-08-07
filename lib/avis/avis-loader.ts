ee9eaf782e349a14b9c409f1799f6109d3236d82
Anas <anas.sahel@gmail.com>
2018-08-07 20:15:11 +0200

    Make static function a member function

--- lib/avis/avis-loader.ts
+++ lib/avis/avis-loader.ts
@@ -17,7 +17,7 @@ export class AvisLoader {
 
   // Get one avis
   async get(id: number, year?: number): Promise<any> {
-    const response = await Axios.get(toUrl(id, year)); // Load the raw avis
+    const response = await Axios.get(this.toUrl(id, year)); // Load the raw avis
     if (response.status === 200) {
       return this.parse(response.data);
     } else {
@@ -34,39 +34,39 @@ export class AvisLoader {
 
     return avis;
   }
-}
 
-export function toId(id: number, year?: number): string {
-  year = year ? year : new Date().getFullYear();
-  let idStr = "" + id;
-  if (idStr.length === 1) idStr = "00" + idStr;
-  else if (idStr.length === 2) idStr = "0" + idStr;
+  async lastId(): Promise<any> {
+    const result = await Axios.get("https://cert.ssi.gouv.fr/avis/feed/");
+    if (result.status === 200) {
+      const $ = Cheerio.load(result.data, {
+        xmlMode: true,
+        normalizeWhitespace: true
+      });
+      const splittedCertId = $("title", $("item").first())
+        .text()
+        .trim()
+        .split(":")[0]
+        .trim()
+        .split("-");
+      return {
+        year: parseInt(splittedCertId[1]),
+        id: parseInt(splittedCertId[3])
+      };
+    } else {
+      return null;
+    }
+  }
 
-  return `CERTFR-${year}-AVI-${idStr}`;
-}
+  toUrl(id: number, year?: number): string {
+    return `https://cert.ssi.gouv.fr/avis/${this.toId(id, year)}/`;
+  }
+  
+  toId(id: number, year?: number): string {
+    year = year ? year : new Date().getFullYear();
+    let idStr = "" + id;
+    if (idStr.length === 1) idStr = "00" + idStr;
+    else if (idStr.length === 2) idStr = "0" + idStr;
 
-export function toUrl(id: number, year?: number): string {
-  return `https://cert.ssi.gouv.fr/avis/${toId(id, year)}/`;
-}
-
-export async function lastId(): Promise<any> {
-  const result = await Axios.get("https://cert.ssi.gouv.fr/avis/feed/");
-  if (result.status === 200) {
-    const $ = Cheerio.load(result.data, {
-      xmlMode: true,
-      normalizeWhitespace: true
-    });
-    const splittedCertId = $("title", $("item").first())
-      .text()
-      .trim()
-      .split(":")[0]
-      .trim()
-      .split("-");
-    return {
-      year: parseInt(splittedCertId[1]),
-      id: parseInt(splittedCertId[3])
-    };
-  } else {
-    return null;
+    return `CERTFR-${year}-AVI-${idStr}`;
   }
 }
